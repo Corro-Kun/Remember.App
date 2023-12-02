af719b0ed5a393c03a87a9efa6c3c5aab71f17a7
Corro-Kun <networpava@gmail.com>
2023-12-02 18:02:41 -0500

    Complement module addSection

--- lib/screens/addSection.dart
+++ lib/screens/addSection.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:remember_app/constans.dart';
 
 class AddSection extends StatelessWidget {
   const AddSection({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(),
+    return const Scaffold(
+      backgroundColor: AppColors.secondaryColor,
+      body: Center(
+        child: Column(
+          children: [
+            Text("Add Section"),
+          ],
+        ),
+      ),
     );
   }
 }
