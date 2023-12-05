4a7d2be2f17dc9ebef49d6b8bf34417ae476943e
Corro-Kun <networpava@gmail.com>
2023-12-05 17:25:54 -0500

    add: addSection with input and button

--- lib/screens/addSection.dart
+++ lib/screens/addSection.dart
@@ -1,17 +1,66 @@
 import 'package:flutter/material.dart';
 import 'package:remember_app/constans.dart';
+import 'package:remember_app/widgets/appBar.dart';
 
 class AddSection extends StatelessWidget {
   const AddSection({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: AppColors.secondaryColor,
+      appBar: const TitleappBar(title: "Añadir Sección"),
       body: Center(
         child: Column(
           children: [
-            Text("Add Section"),
+            Container(
+              margin: const EdgeInsets.only(top: 10, left: 20, right: 20),
+              child: const TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: AppColors.primaryColor,
+                      width: 2.0,
+                    ),
+                  ),
+                  labelText: 'Nombre de la sección...',
+                  labelStyle: TextStyle(
+                    color: AppColors.secondaryTextColor,
+                    fontSize: 17,
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: AppColors.primaryColor,
+                      width: 2.0,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(50),
+              child: GestureDetector(
+                child: Container(
+                  padding: const EdgeInsets.only(
+                    top: 15,
+                    bottom: 15,
+                    left: 20,
+                    right: 20,
+                  ),
+                  decoration: const BoxDecoration(
+                    color: AppColors.primaryColor,
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                  ),
+                  child: const Text(
+                    "Añadir",
+                    style: TextStyle(
+                      color: AppColors.primaryTextColor,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ),
+            )
           ],
         ),
       ),
