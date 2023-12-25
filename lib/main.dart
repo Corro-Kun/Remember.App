a2292478e27ce855378642504e64c1314acb7259
Corro-Kun <networpava@gmail.com>
2023-12-25 17:15:15 -0500

    update vercion and Orientation

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,10 @@ void main() {
       systemNavigationBarColor: AppColors.primaryColor,
     ),
   );
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   runApp(MainApp());
 }
 
