096f32c782a20a21011f37e71ae21b8a5b5ab682
Corro-Kun <networpava@gmail.com>
2024-03-18 16:29:27 -0500

    struct in updateCard

---
+++ lib/screens/updateCard.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter/material.dart';
+
+class updateCard extends StatefulWidget{
+  const updateCard({Key? key}): super(key: key);
+
+  @override
+  State<updateCard> createState() => _updateCard();
+}
+
+class _updateCard extends State<updateCard>{
+
+  @override
+  Widget build(BuildContext context){
+     
+  }
+}
